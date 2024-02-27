f295c658bb862b1c90b82ed98afe7e5105578e4c
Adaobi Chuma-Okeke <chumzine@gmail.com>
2024-02-27 18:36:10 +0100

    Header initiated

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import { LandingPageHeader } from './components/LandingPageHeader';
 
 export function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <LandingPageHeader />
     </div>
   );
 }
